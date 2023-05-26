af4cd9b112c870336259b3d712b5ebe82689d5f1
shankhalam <shankhalamh@gmail.com>
2023-05-26 21:41:50 +0530

    data fetch from a whole year.

--- main.py
+++ main.py
@@ -2,23 +2,33 @@ from flask import Flask, render_template
 import pandas as pd
 
 app = Flask(__name__)
+station = pd.read_csv("data/stations.txt", skiprows=17)
+station = station[["STAID", "STANAME                                 "]]
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
- 
+    return render_template("home.html", data=station.to_html())
+
 
 @app.route("/api/v1/<station>/<date>")
 def data(station, date):
     filename = "data/TG_STAID" + station.zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
-    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze()/10
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {
         "Station": station,
         "Date": date,
         "Temperature": temperature
     }
+
+
+@app.route("/api/v1/<station>")
+def station_data(station):
+    filename = "data/TG_STAID" + station.zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient='records')
+    return result
 
 
 if __name__ == "__main__":
